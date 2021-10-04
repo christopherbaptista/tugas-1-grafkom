ac53a087b8d3a67a296fa3e424c9dfd3090b9d25
Christopher Baptista <57831206+christopherbaptista@users.noreply.github.com>
2021-10-05 01:40:23 +0700

    Update thorn.js

--- thorn.js
+++ thorn.js
@@ -113,7 +113,7 @@ function main() {
     // document.addEventListener('click', onMouseClick, false);
   
     function render(time) {
-      time *= 0.01;
+      time *= 0.0001;
       
       if(!freeze){
         camera.position.y += 0.1;
