c770fe02c9d79f1440c11e24d3b31e2b287d6d57
Christopher Baptista <57831206+christopherbaptista@users.noreply.github.com>
2021-10-05 01:40:43 +0700

    Update thorn.js

--- thorn.js
+++ thorn.js
@@ -115,10 +115,6 @@ function main() {
     function render(time) {
       time *= 0.0001;
       
-      if(!freeze){
-        camera.position.y += 0.1;
-        
-      }
   
       if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
